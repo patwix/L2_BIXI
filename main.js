54cf88e6be32d99753c80c113466e0f8d95844f7
patwix <patwixxx@gmail.com>
2017-10-18 17:20:36 -0400

    Add files via upload
    modification du 17octobre

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Initialisation de la carte google maps
-
 var map;
+
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
@@ -9,8 +9,49 @@ function initMap() {
     });
 }
 
+
 // Initialisation du tableau de la liste des stations
 
+
+let listeStations = {};
+let nomsStations = {};
+
 $(document).ready(function () {
+    chargerStations();
     $("#tableau_liste").DataTable();
 } );
+
+function parseStation(station) {
+    let newStation = {
+        'id': station.id,
+        'nom': station.s,
+        'bloquee': station.b,
+        'suspendue': station.su,
+        'hors-service': station.m,
+        'latitude': station.la,
+        'longitude': station.lo,
+        'bornes-dispo': station.da,
+        'bornes-indispo': station.dx,
+        'velos-dispo': station.ba,
+        'velos-indispo': station.bx,
+    }
+    return newStation;
+}
+
+function chargerStations(){
+    $.get("https://secure.bixi.com/data/stations.json", function(reponse){
+        
+        $.map( reponse.stations, function( val, i ) {
+            let temp = parseStation(val);
+            listeStations[temp.nom] = temp;
+            nomsStations[temp.id] = temp.nom;
+        });
+    });
+}
+
+$(function() {
+    $( "#nom" ).autocomplete({
+        source: nomsStations
+    });
+});
+
