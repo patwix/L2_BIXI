0e78e7e7a7d3b59bb43d4851e449fc2a89f0a419
Mariève Lapierre <Marive Lapierre>
2017-10-30 19:57:56 -0400

    ML: travail terminégit statusgit status

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-// Initialisation de la carte google maps
+// Fonction principale (initialise la carte et charge les informations de bixi)
+
+$(document).ready(function () {
+    initMap();
+    chargerStations();
+} );
+
+
+// Fonctions pour la carte et les marqueurs Google Maps
+
 var map;
+var marker = null;
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
@@ -9,33 +19,28 @@ function initMap() {
 }
 
 function placerMarqueur(coordonnees, nomStation){
-    var marker = new google.maps.Marker({
-        position: coordonnees,
-        map: map,
-        title: nomStation,
+    if (marker != null)
+        marker.setMap(null);
+    marker = new google.maps.Marker({
+    position: coordonnees,
+    map: map,
+    title: nomStation,
     });    
 }
 
 
 // Chargement des informations de la base de donnees de bixi
 
-
 let listeStations = {};
 let stationsAutocomplete = [];
-var stationsTableau = [];
+let stationsTableau = [];
 
-$(document).ready(function () {
-    initMap();
-    chargerStations();
-} );
-
-function initTableau(){
+function initTableau(){             //Initialise le tableau de l'onglet "Liste des stations"
     $('#tableauListe').DataTable({
         
-        /* ca marche pas criss */
-        /* "language": {
+        "language": {
                 "url": "//cdn.datatables.net/plug-ins/1.10.16/i18n/French.json"
-        }*/
+        },
         
         data: stationsTableau,
         columns: [
@@ -49,7 +54,7 @@ function initTableau(){
     });
 }
 
-function parseStation(station) {
+function parseStation(station) {        // Extrait les données pertinentes de la base de données
     let newStation = {
         'id': station.id,
         'nom': station.s,
@@ -66,7 +71,50 @@ function parseStation(station) {
     return newStation;
 }
 
-function chargerStations(){
+function remplirTableauEtat(nomStation){        // Ajoute les données au tableau d'état de la station sélectionnée
+    $( "#IDstation" ).text(listeStations[nomStation].id);
+    if (listeStations[nomStation].bloquee){
+        $( "#bloquee" ).text('Oui');
+        document.getElementById("bloquee").classList.add("fondRouge");
+    }
+    else {
+        $( "#bloquee" ).text('Non');
+        document.getElementById("bloquee").classList.add("fondVert");
+    }  
+    $( "#bornesDis" ).text(listeStations[nomStation].bornesDispo);
+    if (listeStations[nomStation].bornesDispo == 0)
+        document.getElementById("bornesDis").classList.add("fondRouge");
+    else
+        document.getElementById("bornesDis").classList.add("fondVert");
+    
+    if (listeStations[nomStation].suspendue){
+        $( "#suspendu" ).text('Oui');
+        document.getElementById("suspendu").classList.add("fondRouge");
+    }
+    else{
+        $( "#suspendu" ).text('Non');
+        document.getElementById("suspendu").classList.add("fondVert");
+    }
+    $( "#HS" ).text(listeStations[nomStation].horsService);
+    if (listeStations[nomStation].horsService){
+        $( "#HS" ).text('Oui');
+        document.getElementById("HS").classList.add("fondRouge");
+    }
+    else {
+        $( "#HS" ).text('Non');
+        document.getElementById("HS").classList.add("fondVert");
+    }
+    
+    $( "#bornesND" ).text(listeStations[nomStation].bornesIndispo);
+    $( "#veloDispo" ).text(listeStations[nomStation].velosDispo);
+    if (listeStations[nomStation].bornesDispo == 0)
+        document.getElementById("veloDispo").classList.add("fondRouge");
+    else
+        document.getElementById("veloDispo").classList.add("fondVert");
+    $( "#veloND" ).text(listeStations[nomStation].velosIndispo);
+}
+
+function chargerStations(){             // Charge les données et appelle les autres fonctions
     $.get("https://secure.bixi.com/data/stations.json", function(reponse){
         
         $.map( reponse.stations, function( val, i ) {
@@ -74,6 +122,14 @@ function chargerStations(){
             listeStations[temp.nom] = temp;
             stationsAutocomplete[i] = temp.nom;
             stationsTableau[i] = [(temp.id).toString(), temp.nom, (temp.velosDispo).toString(), (temp.bornesDispo).toString(), (temp.bloquee).toString(), (temp.suspendue).toString()];
+            if (stationsTableau[i][4] == "false")
+                stationsTableau[i][4] = "Non";
+            else
+                stationsTableau[i][4] == "Oui";
+                if (stationsTableau[i][5] == "false")
+                stationsTableau[i][5] = "Non";
+            else
+                stationsTableau[i][5] == "Oui";
         });
         initTableau();  
         $(function() {
@@ -83,29 +139,8 @@ function chargerStations(){
                     var nomStation = ui.item.label
                     var coordonnees = {lng: listeStations[nomStation].longitude, lat: listeStations[nomStation].latitude}
                     placerMarqueur(coordonnees, nomStation);
-                    $( "#IDstation" ).text(listeStations[nomStation].id);
-                    
-                    //Vérifie si la variable contient true ou false afin dafficher la bonne affaire tsé...
-                    if (listeStations[nomStation].bloquee)
-                        $( "#bloquee" ).text('Oui');
-                    else
-                        $( "#bloquee" ).text('Non');
-                    $( "#bornesDis" ).text(listeStations[nomStation].bornesDispo);
-                    
-                    if (listeStations[nomStation].suspendue)
-                        $( "#suspendu" ).text('Oui');
-                    else
-                        $( "#suspendu" ).text('Non');
-                    $( "#HS" ).text(listeStations[nomStation].horsService);
-                    if (listeStations[nomStation].horsService)
-                        $( "#HS" ).text('Oui');
-                    else
-                        $( "#HS" ).text('Non');
-                    
-                    
-                    $( "#bornesND" ).text(listeStations[nomStation].bornesIndispo);
-                    $( "#veloDispo" ).text(listeStations[nomStation].velosDispo);
-                    $( "#veloND" ).text(listeStations[nomStation].velosIndispo);
+                    $("#selection").text(nomStation)
+                    remplirTableauEtat(nomStation);                    
                 },
             });
         });              
